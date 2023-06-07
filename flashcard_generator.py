b403a4e66059152e19ad3bd1849306222e7cfb73
lhfdv <lhfranco@protonmail.com>
2023-06-07 14:35:43 -0300

    UI changes, adding languages that I use

--- flashcard_generator.py
+++ flashcard_generator.py
@@ -6,11 +6,11 @@ from tkinter import messagebox
 # Specify the output folder path
 output_folder = "output"
 
-def generate_flashcards(input_text, loading_label, button):
+def generate_flashcards(input_text, language, loading_label, button, progress_bar):
     # GPT Prompt
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"Create anki flashcards for the following Spanish words, only one output per word.: {input_text}. The output format must be: word in spanish;phrase in spanish;word in english;translated phrase. Example: hola;hola, que tal;hello;hello how are you doing"}
+        {"role": "user", "content": f"Create anki flashcards for the following {language} words, only one output per word.: {input_text}. The output format must be: word in spanish;phrase in spanish;word in english;translated phrase. Example: hola;hola, que tal;hello;hello how are you doing"}
     ]
 
     # Update the loading label
@@ -32,10 +32,14 @@ def generate_flashcards(input_text, loading_label, button):
         f.write(generated_flashcards)
 
     # Update the loading label
-    loading_label.config(text="Flashcards generated! Saved to 'output/flashcards.txt'")
+    loading_label.config(text="Saved to 'output/flashcards.txt'")
 
     # Open the flashcards.txt file
     subprocess.Popen(["notepad.exe", file_path])
 
     # Re-enable the button
     button.config(state="normal")
+
+    # Update the progress bar to 100%
+    progress_bar["value"] = 100
+
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 import subprocess
 import threading
 import openai
+
 from dotenv import load_dotenv
+
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from flashcard_generator import generate_flashcards
 
 load_dotenv()
@@ -13,13 +16,18 @@ load_dotenv()
 openai.api_key = os.environ["API_KEY"]
 
 def button_click():
-    input_text = entry.get()
+    input_text = entry.get("1.0", tk.END).strip()  # Retrieve the text from line 1 to the end
+    language = language_combobox.get() # Get the desired language
+
+    if not input_text:
+        tk.messagebox.showerror("Error", "No input")
+        return
 
     # Disable the button while generating flashcards
     window.button.config(state=tk.DISABLED)
 
     # Create a thread for generating flashcards
-    thread = threading.Thread(target=generate_flashcards, args=(input_text, window.loading_label, window.button))
+    thread = threading.Thread(target=generate_flashcards, args=(input_text, language, window.loading_label, window.button, window.progress_bar))
     thread.start()
 
 # Create the main window
@@ -27,21 +35,28 @@ window = tk.Tk()
 window.title("Flashcard Generator")
 
 # Set a fixed size for the window
-window_width = 400
-window_height = 300
+window_width = 580
+window_height = 320
 window.geometry(f"{window_width}x{window_height}")
 
 # Create a label
-label = tk.Label(window, text="Enter words to generate flashcards")
-label.pack()
+label = tk.Label(window, text="Enter words to generate cards:")
+label.pack(pady=2)
+label = tk.Label(window, text="Example: word1, word2, word3, word4")
+label.pack(pady=2)
 
-# Create an input field
-entry = tk.Entry(window)
-entry.pack()
+# Create an input field for words
+entry = tk.Text(window, height=5)
+entry.pack(pady=5)
 
-# Create a button
-window.button = tk.Button(window, text="Generate Flashcards", command=button_click)
-window.button.pack()
+# Create a label for the language selection
+language_label = tk.Label(window, text="Select language:")
+language_label.pack(pady=10)
+
+# Create a Combobox for language selection with default options
+language_combobox = ttk.Combobox(window, values=["Spanish", "French", "Italian", "Japanese"])
+language_combobox.current(0)  # Set the default selection
+language_combobox.pack(pady=5)
 
 # Create a label for the loading message
 loading_label = tk.Label(window, text="")
@@ -49,9 +64,13 @@ window.loading_label = loading_label  # Assign loading_label to window.loading_l
 loading_label.pack()
 
 # Create a progress bar
-progress_bar = ttk.Progressbar(window, mode="indeterminate")
+progress_bar = ttk.Progressbar(window, mode="determinate")
 window.progress_bar = progress_bar  # Assign progress_bar to window.progress_bar
 progress_bar.pack(pady=10)
+
+# Create a button
+window.button = tk.Button(window, text="Generate", command=button_click)
+window.button.pack(pady=10)
 
 # Start the main event loop
 window.mainloop()
